5bed38827b53d478a07f6dd8f11226fd31241e73
phakawan <phakawan@chevron.com>
2018-04-25 17:55:54 +0700

    first commit

---
+++ SeleniumWebTest/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("SeleniumWebTest")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Chevron")> 
+<Assembly: AssemblyProduct("SeleniumWebTest")> 
+<Assembly: AssemblyCopyright("Copyright © Chevron 2018")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("07eb7dd1-8620-4974-b31a-2962e598b7bc")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Remote;
+using System.IO;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private IWebDriver _driverEdge;
+
+        [TestInitialize]
+        public void SetupTest()
+        {
+           
+
+            var options = new EdgeOptions()
+             { 
+                PageLoadStrategy = PageLoadStrategy.Eager
+           
+            };
+
+
+
+            string serverPath = @"C:\work\Devops\driver\edgeLastest";
+          
+            
+            _driverEdge = new EdgeDriver(serverPath,options);
+            _driverEdge.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(5);
+        }
+
+        [TestCleanup]
+        public void TeardownTest()
+        {
+            _driverEdge.Quit();
+        }
+
+
+        [TestMethod]
+        public void RegisterAndSignOff()
+        {
+            string path = @"C:\work\Devops\driver\ie";
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            options.PageLoadStrategy = PageLoadStrategy.Eager;
+            IWebDriver driver = new InternetExplorerDriver(path, options, TimeSpan.FromDays(1));
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromDays(1);
+            driver.Navigate().GoToUrl("http://localhost:64640/Account/Register");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("ctl00$MainContent$UserNameCtrl")).SendKeys("Chevron" + DateTime.Now.ToString("ddmmyyyhhmmss"));
+            driver.FindElement(By.Name("ctl00$MainContent$Password")).SendKeys("ChevronPassword");
+            driver.FindElement(By.Name("ctl00$MainContent$ConfirmPassword")).SendKeys("ChevronPassword");
+            driver.FindElement(By.Name("ctl00$MainContent$ctl08")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 3));
+            wait.Until(wt => wt.FindElement(By.CssSelector(".navbar-right a")));
+            Assert.IsTrue(driver.FindElement(By.CssSelector(".navbar-right a")).Displayed);
+            driver.FindElements(By.CssSelector(".navbar-right a"))[1].Click();
+            driver.Close();
+            driver.Dispose();
+
+        }
+
+        [TestMethod]
+        public void LoginAndLogoff()
+        {
+            string path = @"C:\work\Devops\driver\ie";
+            InternetExplorerOptions options = new InternetExplorerOptions();
+            options.PageLoadStrategy = PageLoadStrategy.Eager;
+            IWebDriver driver = new InternetExplorerDriver(path, options, TimeSpan.FromDays(1));
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromDays(1);
+            driver.Navigate().GoToUrl("http://localhost:64640/Account/Login");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("ctl00$MainContent$UserName")).SendKeys("Chevron");
+            driver.FindElement(By.Name("ctl00$MainContent$Password")).SendKeys("ChevronPassword");
+            driver.FindElement(By.Name("ctl00$MainContent$ctl05")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, new TimeSpan(0, 0, 3));
+            wait.Until(wt => wt.FindElement(By.CssSelector(".navbar-right a")));
+            Assert.IsTrue(driver.FindElement(By.CssSelector(".navbar-right a")).Displayed);
+            driver.FindElements(By.CssSelector(".navbar-right a"))[1].Click();
+            driver.Close();
+            driver.Dispose();
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string path = @"C:\work\Devops\driver\edgeLastest";
+            IWebDriver driver = new EdgeDriver(path);
+            driver.Navigate().GoToUrl("http://localhost:64640/Account/Register");
+            driver.Manage().Window.Maximize();
+            driver.FindElement(By.Name("ctl00$MainContent$UserNameCtrl")).SendKeys("Chevron");
+            driver.FindElement(By.Name("ctl00$MainContent$Password")).SendKeys("ChevronPassword");
+            driver.FindElement(By.Name("ctl00$MainContent$ConfirmPassword")).SendKeys("ChevronPassword");
+            driver.FindElement(By.Name("ctl00$MainContent$ctl08")).Click();
+
+        }
+
+
+
+        [TestMethod]
+        public void Bring()
+        {
+
+            //EdgeOptions capabilities = new EdgeOptions() { BrowserName = "" };
+            //EdgeDriverService service = new EdgeDriverService.Builder().usingDriverExecutable(edgeDriverPath)
+            //    .usingAnyFreePort().build();
+
+            //   string serverPath = @"C:\work\Devops\driver\edgeLastest";
+
+
+            EdgeOptions options = new EdgeOptions();
+            options.UnhandledPromptBehavior = UnhandledPromptBehavior.AcceptAndNotify;
+
+
+
+            //  IWebDriver driver = new EdgeDriver(serverPath);
+            _driverEdge.Url = "https://www.bing.com/";
+            var element = _driverEdge.FindElement(By.Id("sb_form_q"));
+            element.SendKeys("webdriver");
+            element.SendKeys(Keys.Enter);
+            _driverEdge.Quit();
+        }
+
+        [TestMethod]
+        public void TEST()
+        {
+
+            string serverPath = @"C:\work\Devops\driver\edge";
+            EdgeOptions options = new EdgeOptions();
+            options.UnhandledPromptBehavior = UnhandledPromptBehavior.AcceptAndNotify;
+
+            options.PageLoadStrategy = PageLoadStrategy.Eager;
+            IWebDriver driver = new EdgeDriver(serverPath, options);
+            driver.Url = "www.bing.com";
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromDays(1);
+            driver.Navigate();
+        }
+
+    }
+}
